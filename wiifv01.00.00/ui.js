2158084d6395c07c15ca2b6040a4d7dc50f59380
ForsakenPenumbra <opcareermail@gmail.com>
2014-07-22 06:43:51 -0500

    buttons work and home page is done.

---
+++ wiifv01.00.00/ui.js
@@ -0,0 +1,6 @@
+var intro = "testing the intro";
+var about = "testing the about";
+var firstvideo = "https://www.youtube.com/embed/iutsWAjYIqU";
+var secondvideo = "http://www.youtube.com/embed/6dyPPK99Fmk";
+var thirdvideo = "https://www.youtube.com/embed/uu7ELdj54OY";
+var fourthvideo = "https://www.youtube.com/embed/3_KvZ5NSSKw";
